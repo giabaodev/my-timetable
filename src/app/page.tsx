f121f64297ebf90ba59d8b1f663438ec0491a8c2
giabaodev <ngbaodev@gmail.com>
2026-05-05 17:57:08 +0000

    fix: fix proxy on samesite and callback function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,28 @@
-import { PRIVATE_PATHS_NAME } from '@/constants/paths-name';
-import { redirect } from 'next/navigation';
+'use client';
+
+import { PRIVATE_PATHS_NAME, PUBLIC_PATHS_NAME } from '@/constants/paths-name';
+import { useAuthStore, useLoadingStore } from '@/stores';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return redirect(PRIVATE_PATHS_NAME.CALENDAR);
+  const { showLoading, hideLoading } = useLoadingStore();
+  const setUser = useAuthStore((s) => s.setUser);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    showLoading();
+    const userParam = searchParams.get('user');
+    if (userParam) {
+      console.log('User data found in query params, logging in user');
+      const user = JSON.parse(decodeURIComponent(userParam));
+      setUser(user);
+      router.replace(PRIVATE_PATHS_NAME.CALENDAR);
+    } else {
+      console.log('No user data found in query params, redirecting to login');
+      router.replace(PUBLIC_PATHS_NAME.LOGIN);
+    }
+    hideLoading();
+  }, [hideLoading, router, searchParams, setUser, showLoading]);
 }
